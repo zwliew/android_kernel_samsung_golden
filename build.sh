541130a72e0b11ac9d7671dcf65dbac834d0d044
zwliew <zhaoweiliew@gmail.com>
2014-03-16 21:24:07 +0800

    build: Added an intial build script

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo -e "\n-------------------------"
+echo "Executing build script..."
+echo -e "-------------------------"
+
+echo -e "\n\nCleaning kernel source code"
+make mrproper
+
+echo -e "\n\nGenerating defconfig"
+make cm_golden_defconfig
+
+echo -e "\n\nBuilding kernel with duration"
+time make -j5
+
+echo -e \n\n"Copying zImage to generated folder"
+cp arch/arm/boot/zImage generated/zImage
+
+echo -e "\n\nCopying modules to generated folder"
+find . -name "*.ko" -exec cp {} generated \;
+
+echo -e "\n\nDone\n"
